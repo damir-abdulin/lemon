6257e4ad8aac6bdb17246ae9bd160c89919c8419
Damir Abdulin <damir.abdulin@tutanota.com>
2022-05-07 17:23:41 +0300

    Fix slider problems

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,22 +1,45 @@
+const slides = document.getElementsByClassName("hero-page-info");
+const radioButtons = document.getElementsByName("recipe");
+
 let slideIndex = 0;
-let slides = document.getElementsByClassName("hero-page-info");
-let radioButtons = document.getElementsByName("recipe");
+
+if (localStorage.getItem("slideIndex")) {
+    slideIndex = localStorage.getItem("slideIndex") - 1;
+}
+
+for (let i = 0; i < radioButtons.length; i++) {
+    radioButtons[i].addEventListener('change', (event) => {
+        slideIndex = event.target.value;
+        setSlide();
+    });
+}
 
 showSlides();
 
+
 function showSlides() {
-  let i;
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "flex";
-  setRadioButton();
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
 
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+    setRadioButton();
+    setSlide();
+
+    setTimeout(showSlides, 7000); // Change image every 7 seconds
 } 
-
 function setRadioButton() {
     radioButtons[slideIndex-1].checked = true;
+}
+function setSlide() {
+    clearSlider();
+    slides[slideIndex-1].style.display = "flex";
+    localStorage.setItem("slideIndex", slideIndex);
+} 
+
+function clearSlider() {
+    let i;
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
 }
