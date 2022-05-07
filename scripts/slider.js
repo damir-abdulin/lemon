9d2069316a85e4c5bb6ba139ad098bcf1d78c3c7
Damir Abdulin <damir.abdulin@tutanota.com>
2022-05-07 16:52:49 +0300

    Add radio buttons

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,14 +1,22 @@
 let slideIndex = 0;
+let slides = document.getElementsByClassName("hero-page-info");
+let radioButtons = document.getElementsByName("recipe");
+
 showSlides();
 
 function showSlides() {
   let i;
-  let slides = document.getElementsByClassName("hero-page-info");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "flex";
+  setRadioButton();
+
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 } 
+
+function setRadioButton() {
+    radioButtons[slideIndex-1].checked = true;
+}
