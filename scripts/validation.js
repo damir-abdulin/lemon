c0b64a1ec4e1705b948b5324136d1a75584711e7
Damir Abdulin <damir.abdulin@tutanota.com>
2022-05-12 19:44:14 +0300

    Check form in real time

--- scripts/validation.js
+++ scripts/validation.js
@@ -4,28 +4,68 @@ const fieldsSignUp = document.getElementsByClassName('field-sign-up');
 const passwordSignUp = document.getElementById('sign-up-password');
 const repasswordSignUp = document.getElementById('sign-up-repassword');
 const emailSignUp = document.getElementById('sign-up-email');
+const usernameSignUp = document.getElementById('sign-up-username');
 
 const formSignIn = document.getElementById('sign-in');
 const fieldsSignIn = document.getElementsByClassName('field-sign-in');
 
-formSignUp.addEventListener('submit', function (e) {
-    e.preventDefault();
-    
-    removeErrors(formSignUp, 'error-sign-up');
-    checkFieldsBlank(formSignUp, fieldsSignUp, 'error-sign-up');
-    checkPasswordMatch(passwordSignUp, repasswordSignUp, 'error-sign-up');
-    checkEmail(emailSignUp, 'error-sign-up');
+const usernameSignIn = document.getElementById('sign-in-username');
+const passwordSignIn = document.getElementById('sign-in-password');
 
+emailSignUp.addEventListener('input', function(e) {
+    checkEmailSignUp();
+});
+usernameSignUp.addEventListener('input', function(e) {
+    checkSimpleField(formSignUp, usernameSignUp, 'error-username-sign-up');
+});
+passwordSignUp.addEventListener('input', function(e) {
+    checkSimpleField(formSignUp, passwordSignUp, 'error-password-sign-up');
+    checkRepasswordSignUp();
+});
+repasswordSignUp.addEventListener('input', function(e) {
+    checkRepasswordSignUp();
+});
+formSignUp.addEventListener('submit', function (e) {
+    // let result = checkEmailSignUp() && checkSimpleField(usernameSignUp, 'error-username-sign-up') 
+    // checkSimpleField(passwordSignUp, 'error-password-sign-up') && checkRepasswordSignUp();
+
+    let email = checkEmailSignUp();
+    let username = checkSimpleField(formSignUp, usernameSignUp, 'error-username-sign-up');
+    let password = checkSimpleField(formSignUp, passwordSignUp, 'error-password-sign-up');
+    let repassword = checkRepasswordSignUp();
+
+    if (!(email && username && password && repassword)) {
+        e.preventDefault();
+    }
+});
+usernameSignIn.addEventListener('input', function(e) {
+    checkSimpleField(formSignIn, usernameSignIn, 'error-username-sign-in');
+});
+passwordSignIn.addEventListener('input', function(e) {
+    checkSimpleField(formSignIn, passwordSignIn, 'error-password-sign-in');
 });
 formSignIn.addEventListener('submit', function (e) {
-    e.preventDefault();
+    
+    let login = checkSimpleField(formSignIn, usernameSignIn, 'error-username-sign-in');
+    let password = checkSimpleField(formSignIn, passwordSignIn, 'error-password-sign-in');
 
-    removeErrors(formSignIn, 'error-sign-in');
-    checkFieldsBlank(formSignIn, fieldsSignIn, 'error-sign-in');
+    if (!(login && password)) {
+        e.preventDefault();
+    }
 });
 
+function checkSimpleField(form, element, errorClassName) {
+    removeErrors(form, errorClassName);
+    return checkBlanked(element, errorClassName);
+}
+function checkEmailSignUp() {
+    removeErrors(formSignUp, 'error-email-sign-up');
+    
+    let result = checkEmail(emailSignUp, 'error-email-sign-up');
+    result = result && checkBlanked(emailSignUp, 'error-email-sign-up');
 
-
+    return result;
+}
 function getErrorBlock(msg, className) {
     let error = document.createElement('div');
     error.className = className;
@@ -33,6 +73,15 @@ function getErrorBlock(msg, className) {
     error.textContent = msg;
 
     return error;
+}
+function checkBlanked(el, errorClassName) {
+    if (!el.value) {
+        let error = getErrorBlock("Cannot be blank", errorClassName);
+        el.parentElement.insertBefore(error, el.nextSibling);
+
+        return false;
+    }
+    return true;
 }
 function removeErrors(form, errorClassName) {
     let errors = form.getElementsByClassName(errorClassName);
@@ -58,11 +107,9 @@ function checkPasswordMatch(password, repassword, errorClassName) {
     let result = true;
 
     if (password.value != repassword.value) {
-        let errorPassword = getErrorBlock("Password doesn't match", errorClassName);
-        let errorRepassword = getErrorBlock("Password doesn't match", errorClassName);
-
-        password.parentElement.insertBefore(errorPassword, password.nextSibling);       
-        repassword.parentElement.insertBefore(errorRepassword, repassword.nextSibling);
+        let error = getErrorBlock("Password doesn't match", errorClassName);
+   
+        repassword.parentElement.insertBefore(error, repassword.nextSibling);
         result = false;
     }
 
@@ -77,4 +124,11 @@ function checkEmail(email, errorClassName) {
     }
 
     return result;
+}
+function checkRepasswordSignUp() {
+    removeErrors(formSignUp, 'error-repassword-sign-up');
+    let isNotBlank =  checkBlanked(repasswordSignUp, 'error-repassword-sign-up');
+    let isSamePassword = checkPasswordMatch(passwordSignUp, repasswordSignUp, 'error-repassword-sign-up');
+
+    return isNotBlank && isSamePassword;
 }
